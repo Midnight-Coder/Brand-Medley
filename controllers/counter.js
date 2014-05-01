a81960a314975b0265746f6f57845f3c479c3a03
Sagar <jhobalia@yahoo-inc.com>
2014-05-01 04:01:03 -0700

    Adding redirect and reading post params to counter MVC

--- controllers/counter.js
+++ controllers/counter.js
@@ -7,16 +7,16 @@ var CounterModel = require('../models/counter');
 module.exports = function (app) {
 
     app.post("/counter", function(req, res){
-        console.log("POST recevied");
-        console.dir(req);
-        res.redirect('/counter');
+        console.log("POST recevied:" + req.param('brand1') + " & " + req.param('brand2'));
+        var model = {brand1:req.param('brand1').toLowerCase(), brand2: req.param('brand2').toLowerCase()};
+        res.render('counter', model);
     });
 
     // var model = new CounterModel();
 
     app.get('/counter', function (req, res) {
-        res.render('counter', {});
-
+        //Redirect all direct requests to home
+        res.redirect('/');
     });
 
 };
